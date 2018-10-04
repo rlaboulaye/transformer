e3a2007b1c378f0d681c30e276b9ecb482b5ec72
Roland Laboulaye <rlaboulaye@gmail.com>
2018-10-04 00:39:00 -0600

    only create position encodings once

--- dataset.py
+++ dataset.py
@@ -10,8 +10,8 @@ class Dataset(data.Dataset):
 		self.instances = instances
 		self.masks = masks
 		self.targets = targets
-		self.sequence_dim = self.instances.shape[-1]
-		self.vocab_size = vocab_size
+		sequence_dim = self.instances.shape[-1]
+		self.X_positions = np.arange(vocab_size, vocab_size + sequence_dim)
 
 	def __len__(self):
 		return self.instances.shape[0]
@@ -19,10 +19,9 @@ class Dataset(data.Dataset):
 	def __getitem__(self, index):
 		# M is a mask
 		X_tokens = self.instances[index]
-		X_positions = np.arange(self.vocab_size, self.vocab_size + self.sequence_dim)
 		X_value = np.zeros(X_tokens.shape + (2,))
 		X_value[:, :, 0] = X_tokens
-		X_value[:, :, 1] = X_positions
+		X_value[:, :, 1] = self.X_positions
 		X = torch.tensor(X_value, dtype=torch.int64, device=self.device)
 		M = torch.tensor(self.masks[index], dtype=torch.int64, device=self.device)
 		Y = torch.tensor(self.targets[index], dtype=torch.int64, device=self.device)
