ba976cec406ad8a42cdcb52d4028e747a0bd08cd
Roland Laboulaye <rlaboulaye@gmail.com>
2018-10-08 17:16:43 -0600

    modular loss functions

--- loss.py
+++ loss.py
@@ -1,5 +1,25 @@
 import torch
 
+
+def get_language_model_loss(X, Y, M, lm_logits, lm_criterion):
+    X_shifted = X[:, :, 1:, 0].contiguous().view(-1)
+    M = M.view(-1, M.size(2))
+    lm_losses = self.lm_criterion(lm_logits, X_shifted)
+    lm_losses = lm_losses.view(X.size(0) * X.size(1), X.size(2) - 1)
+    lm_losses = lm_losses * M[:, 1:]
+    lm_losses = lm_losses.sum(1) / torch.sum(M[:, 1:], 1)
+    return lm_losses.sum()
+
+def get_task_loss(Y, task_logits, task_criterion):
+    task_losses = task_criterion(task_logits, Y)
+    return task_losses.sum()
+
+def get_double_head_loss(X, Y, M, lm_logits, task_logits, lm_criterion, task_criterion, lm_coef):
+    lm_loss = get_language_model_loss(X, Y, M, lm_logits, lm_criterion)
+    task_loss = get_task_loss(Y, task_logits, task_criterion)
+    double_head_loss = task_loss + lm_coef * lm_loss
+    return double_head_loss, lm_loss, task_loss
+
 class MultipleChoiceLossCompute:
     "A Loss compute and train function for multiple choice tasks."
 
