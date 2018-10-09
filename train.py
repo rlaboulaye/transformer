c34c8e726448e2e958646f45d6ece4b8cbcb0978
Roland Laboulaye <rlaboulaye@gmail.com>
2018-10-09 00:46:41 -0600

    only load model once

--- train.py
+++ train.py
@@ -159,9 +159,9 @@ if __name__ == '__main__':
 	dh_model = DoubleHeadModel(args, text_encoder.classify_token, task_type, vocab_size, sequence_dim)
 
 	#
-	load_openai_pretrained_model(dh_model.transformer, n_ctx=sequence_dim, n_special=3)
-	torch.save(dh_model.state_dict(), 'weights.pth')
-	dh_model = DoubleHeadModel(args, text_encoder.classify_token, task_type, vocab_size, sequence_dim)
+	# load_openai_pretrained_model(dh_model.transformer, n_ctx=sequence_dim, n_special=3)
+	# torch.save(dh_model.state_dict(), 'weights.pth')
+	# dh_model = DoubleHeadModel(args, text_encoder.classify_token, task_type, vocab_size, sequence_dim)
 	dh_model.load_state_dict(torch.load('weights.pth'))
 	#
 
