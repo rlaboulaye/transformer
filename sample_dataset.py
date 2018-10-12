bfe2c69e55b9c3206415ce5e892aa4a2b0ef532e
Mason Poggemann <masonfp@byu.edu>
2018-10-12 15:48:42 -0600

    fixed error so they all samples from all classes

--- sample_dataset.py
+++ sample_dataset.py
@@ -1,22 +1,58 @@
-import pandas as pd
-import random
 import json
 import os
 
-from .data_utils import load_dataframe
+import pandas as pd
+import numpy as np
 
-SCHEMA_DIR = "../schema"
+from data.data_utils import load_dataframe
 
-def sample_datasets(paths=None, types=["DocumentSimilarity"]):
-    if paths is None:
-        _, _, files = os.walk(SCHEMA_DIR)
-        paths = [os.path.join(SCHEMA_DIR, file) for file in files]
+SCHEMA_DIR = "schema/"
 
-    for path in paths:
-        with open(path, "r") as f:
-            task = json.load(f)
-            data = load_dataframe(task["train_file"]["file_path"], task["train_file"]["file_type"], task["train_file"]["file_header"])
-            print(data)
+def sample_dataset(task_path, name=None, num_datasets=None, max_length=300):
+    with open(task_path, "r") as f:
+
+        if name is None:
+            head, tail = os.path.split(task_path)
+            name = tail.replace("_task.json", "").replace(".json", "") + "_sampled"
+        task_dir = os.path.join(head, name)
+        if not os.path.exists(task_dir):
+            os.mkdir(task_dir)
+
+        task = json.load(f)
+        data_head, data_tail = os.path.split(task["train_file"]["file_path"])
+        data_path = os.path.join(data_head, name + "_datasets")
+        if not os.path.exists(data_path):
+            os.mkdir(data_path)
+
+        data = load_dataframe(
+            task["train_file"]["file_path"],
+            task["train_file"]["file_type"],
+            task["train_file"]["file_header"])
+        data = data.sample(frac=1)
+        target = data.columns[task["target"]["column_indices"]][0]
+
+        num_datasets = data.shape[0] // max_length
+        sub_datasets = data.groupby(target)
+        splits = []
+        for _, dataset in sub_datasets:
+            splits.append(np.array_split(dataset, num_datasets))
+        subsets = []
+        for x in range(num_datasets):
+            subset = pd.DataFrame()
+            for split in splits:
+                subset = subset.append(split.pop(0))
+            subsets.append(subset.sample(frac=1))
+
+        for i, dataframe in enumerate(subsets, 1):
+            # file_name = name + "_" + str(i) + "_of_" + str(num_datasets) + ".csv"
+            file_name = f"{name}_{i}_of_{num_datasets}.csv"
+            dataframe.to_csv(os.path.join(data_path, file_name))
+            sub_task = task.copy()
+            sub_task["train_file"]["file_path"] = data_path
+            sub_task["train_file"]["file_type"] = "csv"
+            sub_task["target"]["num_classes"] = dataframe[target].nunique()
+            with open(os.path.join(task_dir, file_name), "w") as fh:
+                json.dump(sub_task, fh, indent=4)
 
 
-sample_datasets()
+sample_dataset("./schema/airline_task.json")
