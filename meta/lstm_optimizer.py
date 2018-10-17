78994dcc65e7fc2e7254c9a2b31f11a89c16353f
Roland Laboulaye <rlaboulaye@gmail.com>
2018-10-16 21:42:26 -0600

    reorganized meta directory

--- meta/lstm_optimizer.py
+++ meta/lstm_optimizer.py
@@ -2,64 +2,82 @@ import numpy as np
 import torch
 from torch import nn
 
+from local_model import LocalModel
+
 
 class LSTMOptimizer(nn.Module):
 
-	def __init__(self, params):
+	def __init__(self, module, hidden_size=20, num_layers=1, momentum=0):
 		super(LSTMOptimizer, self).__init__()
 
 		self.device = 'cpu'
+		self.local_module = module
+		self.momentum = momentum
 
 		self.p = 10
 		self.exp_p = np.exp(self.p)
 		self.neg_exp_p = np.exp(-self.p)
 
-		input_dim = 6
+		self.lstm = nn.LSTM(
+			input_size=4,
+			hidden_size=hidden_size,
+			num_layers=num_layers,
+			bias=True
+			)
+
+		input_dim = hidden_size + 2
 		self.activation = nn.Sigmoid()
 		self.W_theta = nn.Linear(input_dim, 1, bias=True)
 		self.W_grad = nn.Linear(input_dim, 1, bias=True)
-		self.initialize_gates()
+		self.initialize_optimizer_params()
 
-		self.theta_0 = nn.ParameterList()
-		for group in params:
-			self.theta_0.append(nn.Parameter(torch.zeros(group.shape).normal_(0, np.sqrt(2. / sum(group.shape)))))
+		self.theta_0 = nn.Module()
+		for parameter_name in self.local_module._parameters:
+			parameter = self.local_module._parameters[parameter_name]
+			if len(parameter.shape) == 1:
+				setattr(self.theta_0, parameter_name, nn.Parameter(torch.zeros(parameter.shape, device=self.device).normal_(0, .1)))
+			else:
+				setattr(self.theta_0, parameter_name, nn.Parameter(torch.zeros(parameter.shape, device=self.device).normal_(0, np.sqrt(2. / sum(parameter.shape)))))
 
-		self.set_params(params)
+		self.reset_state()
 
-	def initialize_gates(self, window=.01):
+	def initialize_optimizer_params(self, window=.01):
+		for param in self.lstm.parameters():
+			param.data.uniform_(-window, window)
 		self.W_theta.weight.data.uniform_(-window, window)
-		self.W_theta.bias.data.normal_(7., 1.)
+		self.W_theta.bias.data.uniform_(12., 13.)
 		self.W_grad.weight.data.uniform_(-window, window)
-		self.W_grad.bias.data.normal_(-7., 1.)
+		self.W_grad.bias.data.uniform_(-12., -13.)
 
 	def to(self, device):
 		self.device = device
 		super(LSTMOptimizer, self).to(device)
-		for group_index in range(len(self.param_groups)):
-			self.f_tm1[group_index] = self.f_tm1[group_index].to(device)
-			self.i_tm1[group_index] = self.i_tm1[group_index].to(device)
-			self.preprocessed_grad_tm1[group_index] = self.preprocessed_grad_tm1[group_index].to(device)
+		for parameter_name in self.theta_tm1:
+			self.f_tm1[parameter_name] = self.f_tm1[parameter_name].to(device)
+			self.i_tm1[parameter_name] = self.i_tm1[parameter_name].to(device)
+			self.theta_tm1[parameter_name] = self.theta_tm1[parameter_name].to(device)
+			self.delta_tm1[parameter_name] = self.delta_tm1[parameter_name].to(device)
+			if self.state_tm1[parameter_name] is not None:
+				self.state_tm1[parameter_name] = (self.state_tm1[parameter_name][0].to(device), self.state_tm1[parameter_name][1].to(device))
 		return self
 
-	def set_params(self, params):
-		self.param_groups = list(params)
-		self.f_tm1 = []
-		self.i_tm1 = []
-		self.preprocessed_grad_tm1 = []
-		for group in self.param_groups:
-			self.f_tm1.append(torch.ones(group.shape, device=self.device))
-			self.i_tm1.append(torch.zeros(group.shape, device=self.device))
-			self.preprocessed_grad_tm1.append(torch.zeros(group.shape + (2,), device=self.device))
+	def reset_state(self):
+		self.f_tm1 = {}
+		self.i_tm1 = {}
+		self.theta_tm1 = {}
+		self.delta_tm1 = {}
+		self.state_tm1 = {}
+		for parameter_name in self.local_module._parameters:
+			parameter = self.local_module._parameters[parameter_name]
+			self.f_tm1[parameter_name] = torch.ones(parameter.shape, device=self.device)
+			self.i_tm1[parameter_name] = torch.zeros(parameter.shape, device=self.device)
+			self.theta_tm1[parameter_name] = getattr(self.theta_0, parameter_name)
+			self.delta_tm1[parameter_name] = torch.zeros(parameter.shape, device=self.device)
+			self.state_tm1[parameter_name] = None
 
-	def initialize_params(self, params):
-		for group_index, group in enumerate(params):
-			group.data = self.theta_0[group_index].data
-
-	def zero_grad(self):
-		for group in self.param_groups:
-			if group.grad is not None:
-				group.grad.detach_()
-				group.grad.zero_()
+	def initialize_params(self):
+		for parameter_name in self.local_module._parameters:
+			self.local_module._parameters[parameter_name] = getattr(self.theta_0, parameter_name)
 
 	def preprocess(self, x):
 		return torch.cat([self.preprocess_1(x).view(x.shape + (1,)), self.preprocess_2(x).view(x.shape + (1,))], dim=-1)
@@ -90,23 +108,32 @@ class LSTMOptimizer(nn.Module):
 		z[condition_2] = x_2
 		return z
 
-	def update(self, theta_tm1, grad_t, loss, group_index):
-		f_tm1 = self.f_tm1[group_index].view(-1, 1)
-		i_tm1 = self.i_tm1[group_index].view(-1, 1)
-		preprocessed_grad_tm1 = self.preprocessed_grad_tm1[group_index].view(-1, 1)
-		preprocessed_grad_t = self.preprocess(grad_t.view(-1))
-		preprocessed_loss = self.preprocess(loss.view(-1))
-		f_t = self.activation(self.W_theta(torch.cat([preprocessed_grad_t, preprocessed_loss, theta_tm1, f_tm1], dim=-1)))
-		i_t = self.activation(self.W_grad(torch.cat([preprocessed_grad_t, preprocessed_loss, theta_tm1, i_tm1], dim=-1)))
-		theta_t = f_t * theta_tm1 - i_t * grad_t
-		self.f_tm1[group_index] = f_t.view(self.f_tm1[group_index].shape)
-		self.i_tm1[group_index] = i_t.view(self.i_tm1[group_index].shape)
-		# not yet being used
-		self.preprocessed_grad_tm1[group_index] = preprocessed_grad_t.view(self.preprocessed_grad_tm1[group_index].shape)
+	def update_rule(self, grad_t, loss_t, parameter_name):
+		batch_size = grad_t.shape[0]
+		f_tm1 = self.f_tm1[parameter_name].view(-1, 1)
+		i_tm1 = self.i_tm1[parameter_name].view(-1, 1)
+		theta_tm1 = self.theta_tm1[parameter_name].view(-1, 1)
+		delta_tm1 = self.delta_tm1[parameter_name].view(-1, 1)
+		state_tm1 = self.state_tm1[parameter_name]
+		preprocessed_grad_t = self.preprocess(grad_t.view(-1)).view(1, batch_size, -1)
+		preprocessed_loss_t = self.preprocess(loss_t.view(-1)).view(1, batch_size, -1)
+		if state_tm1 is None:
+			output_t, state_t = self.lstm(torch.cat([preprocessed_grad_t, preprocessed_loss_t], dim=-1))
+		else:
+			output_t, state_t = self.lstm(torch.cat([preprocessed_grad_t, preprocessed_loss_t], dim=-1), state_tm1)
+		f_t = self.activation(self.W_theta(torch.cat([output_t.view(batch_size, -1), theta_tm1, f_tm1], dim=-1)))
+		i_t = self.activation(self.W_grad(torch.cat([output_t.view(batch_size, -1), theta_tm1, i_tm1], dim=-1)))
+		delta_t = self.momentum * delta_tm1 - i_t * grad_t
+		theta_t = f_t * theta_tm1 + delta_t
+		self.f_tm1[parameter_name] = f_t.view(self.local_module._parameters[parameter_name].shape)
+		self.i_tm1[parameter_name] = i_t.view(self.local_module._parameters[parameter_name].shape)
+		self.theta_tm1[parameter_name] = theta_t.view(self.local_module._parameters[parameter_name].shape)
+		self.delta_tm1[parameter_name] = delta_t.view(self.local_module._parameters[parameter_name].shape)
+		self.state_tm1[parameter_name] = state_t
 		return theta_t
 
-	def forward(self, loss):
-		for group_index, group in enumerate(self.param_groups):
-			grad = group.grad.detach().view(-1, 1)
-			# need to figure out if i need to individualize loss
-			group.data = self.update(group.view(-1, 1), grad, grad.new_full(grad.shape, loss.item()), group_index).view(group.shape)
+	def forward(self, module_with_grads, loss_t):
+		for parameter_name in module_with_grads._parameters:
+			parameter = module_with_grads._parameters[parameter_name]
+			grad_t = parameter.grad.clone().detach().view(-1, 1)
+			self.local_module._parameters[parameter_name] = self.update_rule(grad_t, grad_t.new_full(grad_t.shape, loss_t.item()), parameter_name).view(parameter.shape)
--- meta/test.py
+++ meta/test.py
@@ -2,37 +2,31 @@ import numpy as np
 import pandas as pd
 import torch
 from torch import nn
-from torch.optim import Adam
+from torch.optim import Adam, SGD
 from torch.nn.modules.loss import MSELoss
 
-from lstm_optimizer import LSTMOptimizer
+from stacked_optimizer import StackedOptimizer
+from mlp import MLP
 
 
-class MLP(nn.Module):
-
-	def __init__(self, input_size, output_size, hidden_size):
-		super(MLP, self).__init__()
-		self.hidden_layer = nn.Linear(input_size, hidden_size, bias=True)
-		self.hidden_activation = nn.ReLU()
-		self.output_layer = nn.Linear(hidden_size, output_size, bias=True)
-
-	def forward(self, x):
-		h = self.hidden_activation(self.hidden_layer(x))
-		return self.output_layer(h)
-
-def train_network(train_set, test_set, optimizer, meta_optimizer, device, epochs):
-
-	epochs = 5
+def train_network(train_set, test_set, loss_function, optimizer, meta_optimizer, device, epochs):
 
 	mlp = MLP(X_Y.shape[-1] - 1, 1, 32)
 	mlp.to(device)
-	loss_function = MSELoss()
 
-	optimizer.set_params(mlp.parameters())
-	optimizer.initialize_params(mlp.parameters())
+	optimizer.reset_state()
+	optimizer.initialize_params(mlp)
+
+	#
+	# optimizer = Adam(mlp.parameters(), lr=.001)
+	# optimizer = SGD(mlp.parameters(), lr=.000003)
+	#
 
 	for epoch in range(epochs):
-		mlp.train()
+		##
+		loss_sum = 0
+		previous_loss = torch.zeros(1, device=device)
+		##
 		losses = []
 		for batch in train_set:
 			x = batch[:,:-1]
@@ -40,41 +34,54 @@ def train_network(train_set, test_set, optimizer, meta_optimizer, device, epochs
 			x = torch.tensor(x, dtype=torch.float32, device=device)
 			y = torch.tensor(y, dtype=torch.float32, device=device)
 			y_hat = mlp(x).view(-1)
-			loss = torch.sqrt(loss_function(y_hat, y))
-			loss.backward(retain_graph=True)
-			# print('Grad: {}'.format(mlp.hidden_layer.weight.grad))
-			# print(mlp.hidden_layer.weight)
-			optimizer.zero_grad()
-			optimizer(loss)
+			loss = loss_function(y_hat, y).sqrt()
+			mlp.zero_grad()
+			loss.backward()
+			# optimizer.step()
+			tuned_mlp = optimizer(mlp, loss)
 			losses.append(loss)
+			##
+			# y_hat = tuned_mlp(x).view(-1)
+			# loss = loss_function(y_hat, y).sqrt()
+			# loss_sum += (loss - previous_loss.detach())
+			# previous_loss = loss.data
+			##
 		losses = torch.cat([loss.unsqueeze(-1) for loss in losses], dim=-1)
 		loss = losses.mean(-1)
-		print('Epoch {}: {}'.format(epoch, loss.cpu().item()))
-		# print(optimizer.W_theta.weight)
-		meta_optimizer.zero_grad()
-		loss.backward()
-		print('Grad: {}'.format(optimizer.W_theta.weight.grad))
-		meta_optimizer.step()
-		# print(optimizer.W_theta.weight)
-	# with torch.no_grad():
-	# 	mlp.eval()
-	# 	losses = []
-	# 	for batch in test_set:
-	# 		x = batch[:,:-1]
-	# 		y = batch[:,-1]
-	# 		x = torch.tensor(x, dtype=torch.float32, device=device)
-	# 		y = torch.tensor(y, dtype=torch.float32, device=device)
-	# 		y_hat = mlp(x).view(-1)
-	# 		loss = torch.sqrt(loss_function(y_hat, y))
-	# 		losses.append(loss)
-	# 	losses = torch.cat([loss.unsqueeze(-1) for loss in losses], dim=-1)
-	# 	loss = losses.mean(-1)
-	# 	return loss
+		print('Epoch {} Train Loss: {}'.format(epoch, loss.cpu().item()))
+		##
+		# meta_optimizer.zero_grad()
+		# loss_sum.backward(retain_graph=True)
+		# meta_optimizer.step()
+		##
+	mlp = tuned_mlp
+	losses = []
+	for batch in test_set:
+		x = batch[:,:-1]
+		y = batch[:,-1]
+		x = torch.tensor(x, dtype=torch.float32, device=device)
+		y = torch.tensor(y, dtype=torch.float32, device=device)
+		y_hat = mlp(x).view(-1)
+		loss = torch.sqrt(loss_function(y_hat, y))
+		losses.append(loss)
+	losses = torch.cat([loss.unsqueeze(-1) for loss in losses], dim=-1)
+	loss = losses.mean(-1)
+	meta_optimizer.zero_grad()
+	loss.backward()
+	# print('Grad: {}'.format(optimizer.optimizers[0].W_theta.weight.grad))
+	# print('Grad: {}'.format(optimizer.optimizers[0].W_grad.weight.grad))
+	# print('Grad: {}'.format(optimizer.optimizers[1].W_theta.weight.grad))
+	# print('Grad: {}'.format(optimizer.optimizers[1].W_grad.weight.grad))
+	meta_optimizer.step()
+	print('Epoch Test Loss: {}'.format(loss.cpu().item()))
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+meta_epochs = 100
+epochs = 20
+
 batch_size = 8
-train_test_split = .7
+train_test_split = .08
 
 df = pd.read_csv('data.csv')
 X_Y = df.values
@@ -82,15 +89,17 @@ X_Y = X_Y.reshape(-1, batch_size, X_Y.shape[-1])
 train_set = X_Y[:round(train_test_split * X_Y.shape[0])]
 test_set = X_Y[round(train_test_split * X_Y.shape[0]):]
 
-mlp = MLP(X_Y.shape[-1] - 1, 1, 32)
-mlp.to(device)
+loss_function = MSELoss()
 
-optimizer = LSTMOptimizer(mlp.parameters())
+mlp = MLP(X_Y.shape[-1] - 1, 1, 32)
+
+optimizer = StackedOptimizer(mlp)
 optimizer.to(device)
 meta_optimizer = Adam(optimizer.parameters(), lr=.001)
 
-meta_epochs = 3
-epochs = 5
 for meta_epoch in range(meta_epochs):
 	print('Meta Epoch {}'.format(meta_epoch))
-	loss = train_network(train_set, test_set, optimizer, meta_optimizer, device, epochs)
+	X_Y = np.random.permutation(X_Y)
+	train_set = X_Y[:round(train_test_split * X_Y.shape[0])]
+	test_set = X_Y[round(train_test_split * X_Y.shape[0]):]
+	train_network(train_set, test_set, loss_function, optimizer, meta_optimizer, device, epochs)
