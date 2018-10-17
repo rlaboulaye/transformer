19ffb8ed457de2ad6b08fe1676594a4bcd5ae5ac
Roland Laboulaye <rlaboulaye@gmail.com>
2018-10-16 18:22:18 -0600

    runs but slowly

--- tmp_meta/test.py
+++ tmp_meta/test.py
@@ -19,7 +19,7 @@ def train_network(train_set, test_set, loss_function, optimizer, meta_optimizer,
 
 	for epoch in range(epochs):
 		losses = []
-		for batch_index, batch in enumerate(train_set):
+		for batch in train_set:
 			x = batch[:,:-1]
 			y = batch[:,-1]
 			x = torch.tensor(x, dtype=torch.float32, device=device)
@@ -30,13 +30,6 @@ def train_network(train_set, test_set, loss_function, optimizer, meta_optimizer,
 			loss.backward()
 			tuned_mlp = optimizer(mlp, loss)
 			losses.append(loss)
-			#
-			# y_hat = tuned_mlp(x).view(-1)
-			# loss = loss_function(y_hat, y).sqrt() - loss.detach()
-			# meta_optimizer.zero_grad()
-			# loss.backward(retain_graph=True)
-			# meta_optimizer.step()
-			#
 		losses = torch.cat([loss.unsqueeze(-1) for loss in losses], dim=-1)
 		loss = losses.mean(-1)
 		print('Epoch {} Train Loss: {}'.format(epoch, loss.cpu().item()))
@@ -53,17 +46,23 @@ def train_network(train_set, test_set, loss_function, optimizer, meta_optimizer,
 	loss = losses.mean(-1)
 	meta_optimizer.zero_grad()
 	loss.backward()
-	print('Grad: {}'.format(optimizer.optimizers[0].W_theta.weight.grad))
+	# print('Grad: {}'.format(optimizer.optimizers[0].W_theta.weight.grad))
+	# print('Grad: {}'.format(optimizer.optimizers[0].W_grad.weight.grad))
+	# print('Grad: {}'.format(optimizer.optimizers[1].W_theta.weight.grad))
+	# print('Grad: {}'.format(optimizer.optimizers[1].W_grad.weight.grad))
 	meta_optimizer.step()
 	print('Epoch Test Loss: {}'.format(loss.cpu().item()))
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-batch_size = 8
-train_test_split = .7
+meta_epochs = 10000
+epochs = 5
+
+batch_size = 2
+train_test_split = .6
 
 df = pd.read_csv('data.csv')
-X_Y = df.values
+X_Y = df.values[:20]
 X_Y = X_Y.reshape(-1, batch_size, X_Y.shape[-1])
 train_set = X_Y[:round(train_test_split * X_Y.shape[0])]
 test_set = X_Y[round(train_test_split * X_Y.shape[0]):]
@@ -76,33 +75,6 @@ optimizer = StackedOptimizer(mlp)
 optimizer.to(device)
 meta_optimizer = Adam(optimizer.parameters(), lr=.001)
 
-###
-# mlp = MLP(X_Y.shape[-1] - 1, 1, 32)
-# mlp.to(device)
-# optimizer.reset_state()
-# optimizer.initialize_params(mlp)
-
-# batch = train_set[0]
-# x = batch[:,:-1]
-# y = batch[:,-1]
-# x = torch.tensor(x, dtype=torch.float32, device=device)
-# y = torch.tensor(y, dtype=torch.float32, device=device)
-# y_hat = mlp(x).view(-1)
-# loss = torch.sqrt(loss_function(y_hat, y))
-# mlp.zero_grad()
-# loss.backward()
-# local_mlp = optimizer(mlp, loss)
-
-# y_hat = local_mlp(x).view(-1)
-# loss = torch.sqrt(loss_function(y_hat, y))
-# meta_optimizer.zero_grad()
-# loss.backward()
-# print(optimizer.optimizers[0].W_theta.weight.grad)
-# meta_optimizer.step()
-###
-
-meta_epochs = 20
-epochs = 5
 for meta_epoch in range(meta_epochs):
 	print('Meta Epoch {}'.format(meta_epoch))
 	train_network(train_set, test_set, loss_function, optimizer, meta_optimizer, device, epochs)
